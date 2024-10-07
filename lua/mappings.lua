602fd710bcbabd661ad11a2cd3cd31602c356344
kongiok <211410146@365.tku.edu.tw>
2024-10-07 23:40:09 +0800

    feat(keymaps): add shortcuts for exiting pane

--- lua/mappings.lua
+++ lua/mappings.lua
@@ -6,6 +6,12 @@ local map = vim.keymap.set
 
 map("n", ";", ":", { desc = "CMD enter command mode" })
 map("i", "jk", "<ESC>")
-map("n", "<D-w>", ":bdlete<CR>", {noremap = true, slient = true})
+-- Exit Page
+--  Exit buffer
+map("n", "<leader>qb", ":bdlete<CR>", { desc = "Close current buffer", noremap = true, silent = true})
+--  Exit Pane
+map("n", "<leader>qp", ":close<CR>", { desc = "Close Current Pane", noremap = true, silent = true})
+--  Exit All
+map("n", "<leader>qq", ":qa!<CR>", {desc = "Close All", noremap = true, silent = false})
 
 -- map({ "n", "i", "v" }, "<C-s>", "<cmd> w <cr>")
